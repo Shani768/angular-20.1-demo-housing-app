622eeb1089d08b69c12055cf304f463a689ac5d2
Shani768 <zeeshantahir779@gmail.com>
2025-07-21 13:57:32 +0500

    first commit

---
+++ src/housing-location/housing-location.ts
@@ -0,0 +1,15 @@
+import { Component,input } from '@angular/core';
+
+import {HousingLocationInfo} from '../housinglocation';
+
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [RouterModule],
+  templateUrl: './housing-location.html',
+})
+export class HousingLocation {
+  housingLocation = input.required<HousingLocationInfo>();
+
+}
